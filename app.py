55bc6d6a38b6d4693a46f46c121c1cb22b5b1568
Kwasi Tabiri <kgtabiri95@gmail.com>
2024-05-18 23:30:36 -0400

    add job items section home page

--- app.py
+++ app.py
@@ -4,10 +4,38 @@ from flask import Flask, render_template
 # create an app (an object of the Flask class)
 app = Flask(__name__)
 
+# simulate a job database
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Scientist',
+        'location': 'Montreal, Quebec',
+        'salary': '$65,000'
+    },
+    {
+        'id': 2,
+        'title': 'Machine Learning Engineer',
+        'location': 'Ottawa, Ontario',
+        'salary': '$75,000'
+    },
+    {
+        'id': 3,
+        'title': 'Data Analyst',
+        'location': 'Guelph, Ontario',
+        'salary': '$80,000'
+    },
+    {
+        'id': 4,
+        'title': 'Backend Developer',
+        'location': 'Regina, Saskatchewan',
+        'salary': '$60,000'
+    }
+]
+
 # register a route (part of the URL after the main name e.g. www.kgtabiri.com/research)
 @app.route("/") # "/" is simply the home or index page
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
